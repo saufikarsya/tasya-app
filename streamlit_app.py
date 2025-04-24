3a7c55eb78ea4c0e6c65b4174351a5a0c75fb142
saufikarsya <saufikaarsya29@gmail.com>
2025-04-24 11:02:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("mama yi papa")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Latihan membuat website melalui Github dan Streamlit"
